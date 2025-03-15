ece89aa588e675d21f8df3e12c201740f19d739f
niloy <chowdhuryniloy819@gmail.com>
2025-03-16 01:15:48 +0600

    added some image grid layout practices

---
+++ Learn_CSS/image_grid2/IMage_grid.js
@@ -0,0 +1,20 @@
+const cl=document.getElementsByClassName('im');
+const dis=document.getElementById('dis');
+ zoom=document.getElementById('zoom');
+
+function func(element){
+    let imgsrc=element.querySelector("img").src;
+    dis.src=imgsrc;
+    zoom.style.display="flex";
+    zoom.style.animationName="zz";
+    // console.log(imgsrc);
+}
+
+function fun2(){
+    zoom.style.animationName="zz2";
+    
+    setTimeout(function(){
+        zoom.style.display="none";
+    },1000);
+    console.log('hi');
+}
