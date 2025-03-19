75cbbe8d6340189614373b7f4a1a173210b38ec5
niloy <chowdhuryniloy819@gmail.com>
2025-03-19 17:40:05 +0600

    Made the website responsive(mobile friendly) and thus the project is done.I can add further modification

--- Practice_Layout/e-commerce/Cloths.js
+++ Practice_Layout/e-commerce/Cloths.js
@@ -53,3 +53,23 @@ function goauto(){
      interx=setInterval(nextx,2000);
 }
 
+const l3 = document.getElementById('l3');
+const l33 = document.getElementById('l33');
+const h3 = document.getElementById('h3');
+
+
+function navbar1() {
+    l3.style.display = 'none';
+    l33.style.display = 'block';
+    h3.style.display = 'flex';
+    h3.style.animationName = 'h3p';
+}
+
+function navbar2() {
+ h3.style.animationName='h3pp';
+    setTimeout(function () {
+        l33.style.display = 'none';
+        l3.style.display = 'block';
+        h3.style.display = 'none';
+    }, 600);
+}
