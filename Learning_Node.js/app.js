589ac862e6e3109516e08e6dd78f703fc81b7c6a
niloy <chowdhuryniloy819@gmail.com>
2025-04-14 00:47:56 +0600

    started to learn node.js

---
+++ Learning_Node.js/app.js
@@ -0,0 +1,21 @@
+// console.log("hello world");
+const _ = require('lodash');
+const obj=require('./index');
+console.log(obj.err(6));
+console.log(_.last(obj.arr));
+
+
+
+
+// console.log(_.last([1,2,3,4]));
+// console.log(global);
+
+// var a=5;
+// console.log(__dirname);
+// console.log(__filename);
+
+// setInterval(() => {
+//     console.log('displaying after every 2s');
+// }, 2000);
+
+
---
+++ Learning_Node.js/index.js
@@ -0,0 +1,15 @@
+var arr=['niloy','adit','siyam'];
+var b=5;
+
+ err=(a)=> {
+  console.log(`there was an error no ${a}`);  
+} 
+
+
+
+module.exports = {
+    arr,
+    b,
+    err
+};
+// console.log(module);
