6fbcda9e0fa37b61a8d081bc59c2a8e4ea4b1bdb
niloy <chowdhuryniloy819@gmail.com>
2025-04-24 01:01:28 +0600

    learned some response properties

---
+++ Learning_Express.js/resObj.js
@@ -0,0 +1,31 @@
+const express = require("express");
+
+const app = express();
+app.use(express.json());
+
+app.set("view engine", "ejs");
+
+app.get("/about", (req, res) => {
+    res.sendStatus(403);
+});
+
+app.listen(3000, () => {
+  console.log("Server is running on port 3000");
+});
+
+// console.log(req.query);
+//     res.render('pages/about',{
+//         Name:req.query.name,
+//         Age:req.query.age,
+//     });
+
+//res.send()-->close response with data
+//res.end()--->close response without data
+
+// res.status(200);
+// res.json({
+//     name: "John Doe",
+//     age: 30,
+//     city: "New York",
+// });
+// res.send('Check the status code 205');
