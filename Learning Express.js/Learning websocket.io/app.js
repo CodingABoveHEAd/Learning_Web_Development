a5ad00fff16146806a4b01a60249701a47afddab
niloy <chowdhuryniloy819@gmail.com>
2025-05-05 03:28:37 +0600

    learned websocket basics for the realtime chat application

---
+++ Learning Express.js/Learning websocket.io/app.js
@@ -0,0 +1,28 @@
+
+const express = require("express");
+const http = require("http");
+const path = require("path");
+const { Server } = require("socket.io");
+
+const app = express();
+const server = http.createServer(app);
+const io=new Server(server);
+
+app.set("view engine", "ejs");
+app.use(express.static(path.join(__dirname, "public")));
+
+app.get("/chat", (req, res) => {
+  res.render("index", { title: "Chat App" });
+});
+
+io.on('connection',(socket)=>{
+    // console.log('A new user connected',socket.id);
+    socket.on('user-message',(message)=>{
+        // console.log('A new user message: ',message);
+        io.emit('message',message);
+    });
+})
+
+server.listen(3000,'0.0.0.0', () => {
+  console.log(`server running on port 3000`);
+});
