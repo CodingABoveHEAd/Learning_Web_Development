d6bf24bec1c984ac4dca4023ab49a5b56abd8446
niloy <chowdhuryniloy819@gmail.com>
2025-05-05 14:38:07 +0600

    made a simple one to many chat application

--- Learning Express.js/Learning websocket.io/app.js
+++ Learning Express.js/Learning websocket.io/app.js
@@ -1,4 +1,3 @@
-
 const express = require("express");
 const http = require("http");
 const path = require("path");
@@ -6,7 +5,7 @@ const { Server } = require("socket.io");
 
 const app = express();
 const server = http.createServer(app);
-const io=new Server(server);
+const io = new Server(server);
 
 app.set("view engine", "ejs");
 app.use(express.static(path.join(__dirname, "public")));
@@ -15,14 +14,14 @@ app.get("/chat", (req, res) => {
   res.render("index", { title: "Chat App" });
 });
 
-io.on('connection',(socket)=>{
-    // console.log('A new user connected',socket.id);
-    socket.on('user-message',(message)=>{
-        // console.log('A new user message: ',message);
-        io.emit('message',message);
-    });
-})
+io.on("connection", (socket) => {
+  // console.log('A new user connected',socket.id);
+  socket.on("user-message", (message) => {
+    // console.log('A new user message: ',message);
+    io.emit("Server_message", {message, serverId: socket.id });
+  });
+});
 
-server.listen(3000,'0.0.0.0', () => {
+server.listen(3000, "0.0.0.0", () => {
   console.log(`server running on port 3000`);
 });
